68fa1908fe65fb7fbae9d32f169bff4a39257703
OstapHetman <prickly4528@gmail.com>
2017-09-12 12:57:38 +0300

    function to close modal if outside click

--- main.js
+++ main.js
@@ -14,10 +14,19 @@ modalBtn.addEventListener('click', openModal);
 //Listen for close click
 closeBtn.addEventListener('click', closeModal);
 
+//Listen for outside click
+window.addEventListener('click', clickOutside);
+
 function openModal() {
     modal.style.display = 'block';
 }
 
 function closeModal() {
     modal.style.display = 'none';
+}
+
+function clickOutside(e) {
+    if (e.target == modal) {
+        modal.style.display = 'none';
+    }
 }
