78305da540818126051bb8a765cdcd5e3dcc274e
OstapHetman <prickly4528@gmail.com>
2017-09-12 12:54:15 +0300

    implement  closeModal

--- main.js
+++ main.js
@@ -6,11 +6,18 @@ var modal = document.getElementById('simpleModal');
 var modalBtn = document.getElementById('modalBtn');
 
 //Get close button
-var closeBtn = document.getElementById('closeBtn');
+var closeBtn = document.getElementsByClassName('closeBtn')[0];
 
-//Listen for click
+//Listen for open click
 modalBtn.addEventListener('click', openModal);
+
+//Listen for close click
+closeBtn.addEventListener('click', closeModal);
 
 function openModal() {
     modal.style.display = 'block';
+}
+
+function closeModal() {
+    modal.style.display = 'none';
 }
